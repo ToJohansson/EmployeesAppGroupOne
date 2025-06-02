27a32616c7bf4e2a12f705a2b6984f5d236b7f2f
Tobias Johansson <tobias@noq.nu>
2025-06-02 14:48:32 +0200

    Add Salary property to Employee and update migrations
    - Introduced a nullable `Salary` property in the `Employee` class.
    - Updated migration files to add the `Salary` column, changing its type from `decimal(18,2)` to `money`.
    - Modified `ApplicationContextModelSnapshot` to include the new `Salary` property.
    - Configured the `Salary` column type as `money` in the `ApplicationContext`.
    - Changed service registration in `Program.cs` from `AddSingleton` to `AddScoped` for `IEmployeeRepository` and `IEmployeeService`.

---
+++ EmployeesApp.Infrastructure/Migrations/20250602124104_init.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EmployeesApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Email", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Stockholm@hotmail.com", "Ica" },
+                    { 3, "Göteborg@hotmail.com", "Hemköp" },
+                    { 22, "Stockholm@hotmail.com", "Coop" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
